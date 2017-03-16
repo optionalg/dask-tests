6ddbb5e3174188ee2f00c9bf669e1d8fedd3cbe2
Hugo Bowne-Anderson <hugo@datacamp.com>
2017-03-16 14:09:01 -0400

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+##### Install specific package versions with pip #####
+
+pip3 install pandas
+pip3 install graphviz
+pip3 install cloudpickle
+pip3 install dask distributed
+pip3 install requests
+pip3 install matplotlib
+pip3 install toolz
+pip3 install pytables
+pip3 install bokeh
