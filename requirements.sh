f67f0892ddb5d323f9b7c0b9a374e28437e21409
Hugo Bowne-Anderson <hugo@datacamp.com>
2017-03-22 11:37:50 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,7 +3,7 @@
 pip3 install pandas
 pip3 install graphviz
 pip3 install cloudpickle
-pip3 install dask distributed
+pip3 nstall dask[complete] distributed
 pip3 install requests
 pip3 install matplotlib
 pip3 install toolz
