233c1bb43baddbe21ef2e5272df99501f54e19c9
Hugo Bowne-Anderson <hugo@datacamp.com>
2017-08-30 09:28:54 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,13 +1,3 @@
 ##### Install specific package versions with pip #####
 
-pip install altair
-pip3 install pandas
-pip3 install graphviz
-pip3 install cloudpickle
-pip3 install dask[complete] distributed
-pip3 install requests
-pip3 install matplotlib
-pip3 install toolz
-pip3 install pytables
-pip3 install bokeh
-pip install pymc3
+pip3 install pymc3
